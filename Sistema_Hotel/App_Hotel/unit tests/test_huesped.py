ee4312100cf513d1c58f0558c3b8d1dcffff90ab
dparedesp3 <jloorm4@gmail.com>
2024-08-13 18:34:10 -0500

    Update test_huesped.py

--- Sistema_Hotel/App_Hotel/unit tests/test_huesped.py
+++ Sistema_Hotel/App_Hotel/unit tests/test_huesped.py
@@ -19,3 +19,27 @@ def test_create_huesped_service():
     huesped = service.create_huesped("Pablo", "prueba@correo.com")
     assert huesped.nombre == "Pablo"
     assert huesped.email == "prueba@correo.com"
+    
+# Pruebas de Repositorios
+@pytest.mark.django_db
+def test_get_all_huespedes_repository():
+    repository = HuespedRepository()
+    huespedes = repository.get_all()
+    assert len(huespedes) == 0  # Asumiendo que la base de datos esté vacía al inicio de la prueba
+
+# Pruebas de Controladores (ViewSets)
+@pytest.mark.django_db
+def test_get_huespedes_viewset():
+    client = APIClient()
+    response = client.get(reverse('huesped-list'))
+    assert response.status_code == 200
+    assert response.json() == []
+
+@pytest.mark.django_db
+def test_create_huesped_viewset():
+    client = APIClient()
+    data = {'nombre': 'Sergio', "email": 'correodeprueba@gmail.com'}
+    response = client.post(reverse('huesped-list'), data)
+    assert response.status_code == 201
+    assert response.json()['nombre'] == 'Sergio'
+    assert response.json()['email'] == 'correodeprueba@gmail.com'
